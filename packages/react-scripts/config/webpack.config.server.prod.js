8feaa77e62a758d03101bd4763c9d6dea977717c
Jonatan Lundin <jonatan.lundin@gmail.com>
2017-07-27 11:14:07 +0200

    Remove uglify from server bundle

--- packages/react-scripts/config/webpack.config.server.prod.js
+++ packages/react-scripts/config/webpack.config.server.prod.js
@@ -201,24 +201,6 @@ module.exports = {
         'process.env.BUILD_TARGET': JSON.stringify('server'),
       })
     ),
-    // Minify the code.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        // Disabled because of an issue with Uglify breaking seemingly valid code:
-        // https://github.com/facebookincubator/create-react-app/issues/2376
-        // Pending further investigation:
-        // https://github.com/mishoo/UglifyJS2/issues/2011
-        comparisons: false,
-      },
-      output: {
-        comments: false,
-        // Turned on because emoji and regex is not minified properly using default
-        // https://github.com/facebookincubator/create-react-app/issues/2488
-        ascii_only: true,
-      },
-      sourceMap: true,
-    }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
